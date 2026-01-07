245d74861166e4cdee4c0cdb1505efad9e00347d
Evan Kim <evandde@gmail.com>
2026-01-07 18:59:18 +0900

    refactor: use name-first variable naming in DetectorConstruction

--- cookiecutter-g4starter/{{cookiecutter.project_name}}/src/DetectorConstruction.cc
+++ cookiecutter-g4starter/{{cookiecutter.project_name}}/src/DetectorConstruction.cc
@@ -10,15 +10,15 @@ G4VPhysicalVolume *DetectorConstruction::Construct()
 {
     // materials
     auto nist = G4NistManager::Instance();
-    auto matAir = nist->FindOrBuildMaterial("G4_AIR");
+    auto airMat = nist->FindOrBuildMaterial("G4_AIR");
 
     // World
     auto worldSize = 1. * m;
-    auto solWorld = new G4Box("World", .5 * worldSize, .5 * worldSize, .5 * worldSize);
-    auto lvWorld = new G4LogicalVolume(solWorld, matAir, "World");
-    auto pvWorld = new G4PVPlacement(0, G4ThreeVector(), lvWorld, "World", nullptr, false, 0);
+    auto worldSol = new G4Box("World", .5 * worldSize, .5 * worldSize, .5 * worldSize);
+    auto worldLog = new G4LogicalVolume(worldSol, airMat, "World");
+    auto worldPhys = new G4PVPlacement(0, G4ThreeVector(), worldLog, "World", nullptr, false, 0);
 
-    return pvWorld;
+    return worldPhys;
 }
 
 void DetectorConstruction::ConstructSDandField()
